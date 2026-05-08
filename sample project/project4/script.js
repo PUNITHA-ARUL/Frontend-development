fe9a6dfd68ff7e4ba55c3b9d839e181c7e9f16b7
PUNITHA-ARUL <arulpunitha2007@gmail.com>
2026-05-08 22:29:55 +0530

    add javascript

---
+++ sample project/project4/script.js
@@ -0,0 +1,9 @@
+var popupoverlay =document.querySelector(".popup-overlay")
+var popupcreate=document.querySelector(".popup-create")
+var addpopbutton=document.getElementById("add-popup-button")
+
+addpopbutton.addEventListener("click",function()
+{
+    popupoverlay.style.display="block"
+    popupcreate.style.display="block"
+})
