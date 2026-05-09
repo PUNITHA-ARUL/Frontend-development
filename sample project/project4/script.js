615640a9243b7add6f8714923929c1f7572cbc79
PUNITHA-ARUL <arulpunitha2007@gmail.com>
2026-05-09 17:35:58 +0530

    add cancel

--- sample project/project4/script.js
+++ sample project/project4/script.js
@@ -1,9 +1,18 @@
 var popupoverlay =document.querySelector(".popup-overlay")
 var popupcreate=document.querySelector(".popup-create")
 var addpopbutton=document.getElementById("add-popup-button")
-
-addpopbutton.addEventListener("click",function()
+ addpopbutton.addEventListener("click",function()
 {
     popupoverlay.style.display="block"
     popupcreate.style.display="block"
 })
+var cancelpopbutton=document.getElementById("cancel-popup")
+cancelpopbutton.addEventListener("click",function(event)
+{
+  event.preventDefault()
+  popupoverlay.style.display="none"
+  popupcreate.style.display="none"
+
+}
+
+)
