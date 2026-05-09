add66be30fd4ffe66e2a5bea3fd88be946296ccf
PUNITHA-ARUL <arulpunitha2007@gmail.com>
2026-05-09 22:06:28 +0530

    add javasrcipt

--- sample project/project4/script.js
+++ sample project/project4/script.js
@@ -13,6 +13,34 @@ cancelpopbutton.addEventListener("click",function(event)
   popupoverlay.style.display="none"
   popupcreate.style.display="none"
 
-}
+})
 
-)
+//add button click and automatically create the popupbox
+//select popup,book-title-input,book-author-input,book-description,add-book
+
+var popup= document.querySelector(".popup")
+var booktitle= document.getElementById("book-title-input")
+var bookauthor= document.getElementById("book-author-input")
+var bookdescription= document.getElementById("book-description")
+var addbook= document.getElementById("add-book")
+
+addbook.addEventListener("click",function(event)
+{
+  event.preventDefault()
+  var div=document.createElement("div")
+  div.setAttribute("class","book-popup")
+  div.innerHTML=`<h2>${booktitle.value}</h2>
+  <h5>${bookauthor.value}</h5>
+  <p>${bookdescription.value}</p>
+  <button onclick="deletebook(event)">Delete</button>`
+  popup.append(div)   
+  popupoverlay.style.display="none"
+  popupcreate.style.display="none"  
+        
+
+})
+
+function deletebook(event)
+{
+  event.target.parentElement.remove() 
+}
