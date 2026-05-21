0c7d62fe796426c37856f6a20a19e116b35ca79e
PUNITHA-ARUL <arulpunitha2007@gmail.com>
2026-05-21 12:48:44 +0530

    add temperature

--- counter people/Function pracitce/funtionitro.js
+++ counter people/Function pracitce/funtionitro.js
@@ -1,15 +1,27 @@
-function sayhello()
+// Today celsius
+function celsius()
 {
-    return 'Hello'
+    return 'Today Celsius:'
 }
 
-let say = sayhello();      
+let weather = celsius();      
 
-console.log(say);
-
+console.log(weather);
+// add celsius
 function add(a,b)
 {
     return(a+b)
 }
-let sum=add(20,30)
+let sum=add(40,30)
 console.log(sum)
+
+//varialbe used to store the return value of a function
+// text of temperature
+let text = "The temperature "+  weather1(77) +" Celsius";
+
+function weather1(frhrenheit)
+{
+    return (5/9)* (frhrenheit-32)
+}
+
+console.log(text)
