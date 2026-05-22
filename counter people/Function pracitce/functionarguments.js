076541d23334f284600f9fc1cc0ea5f19beee0a2
PUNITHA-ARUL <arulpunitha2007@gmail.com>
2026-05-22 10:13:57 +0530

    largest even number

--- counter people/Function pracitce/functionarguments.js
+++ counter people/Function pracitce/functionarguments.js
@@ -1,47 +1,55 @@
 // maximum number of argument object
-function findmax()
-{
-    let max= -Infinity
-    for(i = 0; i < arguments.length; i++)
-    {
-        if(arguments[i] > max)
-        {
-            max= arguments[i]
+function findmax() {
+    let max = -Infinity
+    for (i = 0; i < arguments.length; i++) {
+        if (arguments[i] > max) {
+            max = arguments[i]
         }
     }
     return max
 }
 
-console.log('Here your infinity number', + findmax(300,400,500))
+console.log('Here your infinity number', + findmax(300, 400, 500))
 
 // minimum number of argument object
-function findmin()
-{
-   let  min= Infinity
-    for( i=0; i < arguments.length; i++)
-    {
-        if(arguments[i] < min)
-        {
-            min= arguments[i]
+function findmin() {
+    let min = Infinity
+    for (i = 0; i < arguments.length; i++) {
+        if (arguments[i] < min) {
+            min = arguments[i]
         }
     }
-return min
+    return min
 }
-console.log('Here your infinity number', + findmin(300,400,500))
+console.log('Here your infinity number', + findmin(300, 400, 500))
 
 // sum of the number 
-function findsum(a,b,c)
-{
-    return (a+b+c)
+function findsum(a, b, c) {
+    return (a + b + c)
 }
-console.log('Here your infinity number', + findsum(300,400,500))
+console.log('Here your infinity number', + findsum(300, 400, 500))
 
 // count the number length
-function findcount()
-{
-    for(i=0; i= arguments.length; i++)
-    {
+function findcount() {
+    for (i = 0; i = arguments.length; i++) {
         return (i)
     }
 }
-console.log('Here your infinity number', + findcount(300,400,500))
+console.log('Here your infinity number', + findcount(300, 400, 500))
+
+// largest even number
+
+function findeven() {
+    max =  -Infinity
+    for (i = 0; i < arguments.length; i++) {
+        if (arguments[i] > max) {
+            if(arguments[i] % 2 == 0){
+                max = arguments[i]
+
+            }
+        }
+    }
+    return max
+}
+console.log('Here your infinity number', + findeven(300, 400, 500))
+
