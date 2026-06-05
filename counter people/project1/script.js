9944b5ab4671b963ef2fcf0b6a0627e1fab41831
PUNITHA-ARUL <arulpunitha2007@gmail.com>
2026-06-05 11:21:37 +0530

    modified stdcard

---
+++ counter people/project1/script.js
@@ -0,0 +1,7 @@
+
+function myFunction(name, age, grade){
+    console.log("My name is " + name + ", I am " + age + " years old, and I am in grade " + grade + ".");
+}
+myFunction("Alice", 20, 12);
+myFunction("Bob", 22, 11);
+myFunction("Charlie", 19, 10);
